ee0061d3c6e153304cf320e420b8cf8c54a30fb0
Your Name <you@example.com>
2023-02-28 21:56:27 +0700

    add detail page

--- tmdt_client/src/component/detailpage/detailpage.jsx
+++ tmdt_client/src/component/detailpage/detailpage.jsx
@@ -1,9 +1,28 @@
-import InfoProduct from "./sestionpage/sestionpage"
-
-const DetailPage=()=>{
-    return(
+import React from "react";
+import { Component } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import InfoProduct from "./sestionpage/sestionpage";
+import { useParams } from "react-router-dom";
+const DetailPage = (props) => {
+    var {id} = useParams()
+    console.log(id);
+    const [SinhVien, setSinhVien] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:8000/edit/'+id)
+            .then(res =>{
+                setSinhVien(res.data)
+            } )
+            .catch(err => console.log(err))
+    }, [])
+    const Tim = () => {
+        axios.get('http://localhost:8000/edit/' + id)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
+    }
+    return (
         <div className="detailpage">
-            <InfoProduct/>
+            <InfoProduct detail={SinhVien} />
         </div>
     )
 }
